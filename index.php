487ea9f193a3f2b2c0942ccbb92d1739c772de4f
tkmini <tkmini@192.168.3.3>
2020-04-23 18:12:54 +0900

    新規

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>たかPortfolio</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+</head>
+
+<body id="page-top"><a class="menu-toggle rounded" href="#"><i class="fa fa-bars"></i></a>
+    <nav class="navbar navbar-light navbar-expand" id="sidebar-wrapper">
+        <div class="container"><button data-toggle="collapse" class="navbar-toggler d-none" data-target="#"></button>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav sidebar-nav" id="sidebar-nav">
+                    <li class="nav-item sidebar-brand" role="presentation"><a class="nav-link active js-scroll-trigger" href="#page-top">たかPortfolio</a></li>
+                    <li class="nav-item sidebar-nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#page-top">HOME</a></li>
+                    <li class="nav-item sidebar-nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#about">PROFIL</a></li>
+                    <li class="nav-item sidebar-nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#services">WORKS</a></li>
+                    <li class="nav-item sidebar-nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#portfolio">PORTFOLIO</a></li>
+                    <li class="nav-item sidebar-nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#contact">CONTACT</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header class="d-flex masthead" style="background-image:url('assets/img/bg-masthead.jpg');">
+        <div class="container my-auto text-center">
+            <h1 class="mb-1">Taka&nbsp;Portfolio</h1>
+            <h3 class="mb-5"><em>IT Enginner / Web design</em></h3><a class="btn btn-primary btn-xl js-scroll-trigger" role="button" href="#about">Find Out More</a>
+            <div class="overlay"></div>
+        </div>
+    </header>
+    <section id="about" class="content-section bg-light">
+        <div class="container">
+            <div class="content-section-heading">
+                <h3 class="text-center text-secondary mb-0">PROFIL</h3>
+                <h2 class="text-center mb-5"><strong>自己紹介</strong></h2>
+            </div>
+            <div class="row">
+                <div class="col text-center mb-5 mb-lg-0" style="margin-top: 70px;"><img src="assets/img/my.png"></div>
+                <div class="col mb-5 mb-lg-0" style="margin-top: 70px;">
+                    <div>
+                        <div class="col-md-6 col-lg-auto mb-5 mb-lg-0">
+                            <ul>
+                                <li style="font-size: 20px;margin-bottom: 10px;">名前: Takai Hiroshi</li>
+                                <li style="font-size: 20px;margin-bottom: 10px;">出身: Osaka</li>
+                                <li style="font-size: 20px;margin-bottom: 10px;">生年月日: 1982 / 03 / 31</li>
+                                <li style="font-size: 20px;">活動:
+新卒では某OAメーカーの営業で入社し、3年ほどで退社。その後、中小企業のSierに入社。正社員として常駐案件を4年ほど経て、フリーランスになりました。
+フリーになってから10年以上、SE業界でフロント・バックエンドを多岐にわたって経験し、現在はフルスタックエンジニアとしての活動が主体となります。
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col text-center mb-5 mb-lg-0" style="margin-top: 70px;"><a class="btn btn-dark btn-xl js-scroll-trigger" role="button" href="#services">Works</a></div>
+            </div>
+        </div>
+    </section>
+    <section id="services" class="content-section text-white text-center" style="background-color: #008ed6;">
+        <div class="container">
+            <div class="content-section-heading">
+                <h3 class="text-secondary mb-0">Works</h3>
+                <h2 class="mb-5">仕事内容</h2>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-lg-3 mb-5 mb-lg-0"><span class="mx-auto service-icon rounded-circle mb-3"><i class="fas fa-desktop"></i></span>
+                    <h5><strong>エンジニア案件</strong></h5>
+                    <p class="mb-0 text-faded" style="font-size: 12px;">主にシステムエンジニアとして活動しており、業務システムの構築などの案件に参画し、常駐・リモートワークで対応しております。</p><a class="btn btn-success btn-lg" role="button" href="skillset.php" style="margin: 8px;" target="_blank">スキル・経歴</a></div>
+                <div class="col-md-6 col-lg-3 mb-5 mb-lg-0"><span class="mx-auto service-icon rounded-circle mb-3"><i class="fas fa-user-tie"></i></span>
+                    <h5><strong>ITコンサルティング</strong></h5>
+                    <p class="mb-0 text-faded" style="font-size: 12px;">スタートアップ企業に参画した経験を生かして、業務効率化における様々な課題をITソリューションで問題解決致します。
+                    </p>
+                </div>
+                <div class="col-md-6 col-lg-3 mb-5 mb-lg-0"><span class="mx-auto service-icon rounded-circle mb-3"><i class="fas fa-laptop-code"></i></span>
+                    <h5><strong>Web制作</strong></h5>
+                    <p class="mb-0 text-faded" style="font-size: 12px;">Web制作を立案からメンテナンスまで、Webサービスをお客様がどう運営してくかまでを見据え、ビジネス課題を解決するためのWeb戦略を具現化します。</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="portfolio" class="content-section">
+        <div class="container">
+            <div class="content-section-heading text-center">
+                <h3 class="text-secondary mb-0">Portfolio</h3>
+                <h2 class="mb-5">制作実績</h2>
+            </div>
+            <div class="row no-gutters">
+                <!-- jog 700,467 -->
+                <div class="col-lg-6">
+                    <a class="portfolio-item" href="#" target="_blank">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <h5>ポートフォリオ</h5>
+                                <p class="mb-0" style="font-size: 15px;">自分自身のポートフォリオです。</p>
+                                <p class="mb-0">HTML5 / CSS / Bootstrap4.4 </p>
+                            </div>
+                        </div><img class="img-fluid" src="assets/img/portfolio2020-1.png"></a>
+                </div>
+                <div class="col-lg-6">
+                    <a class="portfolio-item" href="https://p-stage.com/todo/" target="_blank">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <h5>TODOタスク管理</h5>
+                                <p class="mb-0" style="font-size: 15px;">認証機能を実装した、タスク管理アプリです。</p>
+                                <p class="mb-0">PHP7.2 / Laravel / Bootstrap4 / jQuery</p>
+                            </div>
+                        </div><img class="img-fluid" src="assets/img/portfolio2020-2.png"></a>
+                </div>
+                <!--
+                <div class="col-lg-6">
+                    <a class="portfolio-item" href="#">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <h2>Strawberries</h2>
+                                <p class="mb-0">Strawberries are such a tasty snack, especially with a little sugar on top!</p>
+                            </div>
+                        </div><img class="img-fluid" src="assets/img/portfolio-3.jpg"></a>
+                </div>
+                <div class="col-lg-6">
+                    <a class="portfolio-item" href="#">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <h2>Workspace</h2>
+                                <p class="mb-0">A yellow workspace with some scissors, pencils, and other objects.</p>
+                            </div>
+                        </div><img class="img-fluid" src="assets/img/portfolio-4.jpg"></a>
+                </div>
+                 -->
+            </div>
+        </div>
+    </section>
+    <section id="contact">
+        <footer class="footer text-center">
+            <div class="container">
+                <div class="content-section-heading text-center">
+                    <h3 class="text-secondary mb-0">Contact</h3>
+                    <h2 class="mb-5">お問い合わせ・SNS</h2>
+                </div>
+                <span>メール・TwitterのDMから、お気軽にご連絡ください。</span>
+                <ul class="list-inline mb-5">
+                    <li class="list-inline-item">&nbsp;
+                        <a class="text-white social-link rounded-circle" href="mailto:h-takai@p-stage.com?subject=問い合わせ&amp;body=ご記入ください">
+                            <i class="fas fa-envelope"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">&nbsp;
+                        <a class="text-white social-link rounded-circle" href="https://twitter.com/takatyu_hiro" target="_blank">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">&nbsp;
+                        <a class="text-white social-link rounded-circle" href="https://github.com/takatyu" target="_blank">
+                            <i class="fab fa-github"></i>
+                        </a>
+                    </li>
+                </ul>
+                <p class="text-muted mb-0 small">Copyright &nbsp;© たかPortfolio 2020</p>
+            </div><a class="js-scroll-trigger scroll-to-top rounded" href="#page-top" style="display: inline;"><i class="fa fa-angle-up"></i></a></footer>
+    </section>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="assets/js/stylish-portfolio.js"></script>
+
+
+</body></html>
---
+++ skillset.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Skill Set</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets/css/table.css">
+</head>
+
+<body>
+    <section class="content-section text-center" style="padding: 50px;background-color: #d8dbcc;">
+        <h2 class="mb-5">スキルセット</h2>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+                    <h5 style="background-color: #75aadb;">言語</h5>
+                    <ul style="color: rgb(33,37,41);">
+                        <li class="text-left">Java</li>
+                        <li class="text-left">C#</li>
+                        <li class="text-left">VB.Net</li>
+                        <li class="text-left">PHP 5系/7系</li>
+                        <li class="text-left">JavaScript</li>
+                        <li class="text-left">Python</li>
+                        <li class="text-left">HTML5</li>
+                        <li class="text-left">PL/SQL</li>
+                    </ul>
+                </div>
+                <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+                    <h5 style="background-color: #75aadb;">FW / MW</h5>
+                    <ul style="color: rgb(33,37,41);">
+                        <li class="text-left">Struts 1 / 2</li>
+                        <li class="text-left">Seasar2</li>
+                        <li class="text-left">WebOTX</li>
+                        <li class="text-left">あらまほ</li>
+                        <li class="text-left">Justware</li>
+                    </ul>
+                </div>
+                <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+                    <h5 style="background-color: #75aadb;">OS・サーバ</h5>
+                    <ul style="color: rgb(33,37,41);">
+                        <li class="text-left">Windows</li>
+                        <li class="text-left">Mac OS</li>
+                        <li class="text-left">Linux(RedHat/CentOS/)</li>
+                        <li class="text-left">Uinux(AIX/FreeBSD/Solaris)</li>
+                    </ul>
+                </div>
+                <div class="col-md-6 col-lg-3 mb-5 mb-lg-0">
+                    <h5 style="background-color: #75aadb;">その他</h5>
+                    <ul style="color: rgb(33,37,41);">
+                        <li class="text-left">Oracle</li>
+                        <li class="text-left">MySql</li>
+                        <li class="text-left">PostgreSQL</li>
+                        <li class="text-left">DB2</li>
+                        <li class="text-left">Cache</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div>
+        <h2 class="text-center mb-5" style="margin: 30px;">過去の実績</h2>
+        <div style="margin: 35px;">
+            <div class="table-responsive table-borderless">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th class="text-white bg-primary">システム名</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th class="table-warning"><strong>業務内容</strong></th>
+                        </tr>
+                        <tr>
+                            <td style="padding: 10px;padding-left: 20px;">小売業　サーバ移設案件</td>
+                        </tr>
+                        <tr>
+                            <th class="table-warning"><strong>担当業務・規模</strong></th>
+                        </tr>
+                        <tr>
+                            <td style="padding: 10px;padding-left: 20px;">
+                                計画/要件整理 ～ リリース<br/>
+                                バージョンアップに伴う調整業務とPG改修を担当<br>
+                                チーム全体：5名</td>
+                        </tr>
+                        <tr>
+                            <th class="table-warning"><strong>担当業務・規模</strong></th>
+                        </tr>
+                        <tr>
+                            <td style="padding: 10px;padding-left: 20px;">
+                                <ul>
+                                    <li>OS：Windows</li>
+                                    <li>言語：Java / C#</li>
+                                    <li>FW：Spring / Struts2 / .Net</li>
+                                    <li>DB：MySQL</li>
+                                    <li>Server：CentOS 7系 / Windows Server2012 R2
+                                    </li>
+                                </ul>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        <strong style="color: red;"> ※2020/04/23 更新記載途中</strong>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="assets/js/stylish-portfolio.js"></script>
+
+
+</body>
+</html>
